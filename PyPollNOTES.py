5be505d08ab5f9e669620839aa6a1d68c26c6f7c
russgo1 <russellg580@gmail.com>
2022-10-19 12:37:15 -0400

    through 3.4.5

--- PyPollNOTES.py
+++ PyPollNOTES.py
@@ -4,7 +4,44 @@
 # 3. The percentage of votes each candidate won
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on popular vote.
+'''
+import datetime as dt
+from inspect import FullArgSpec
 
+print(dt.datetime.now())
+
+import csv
+###
+# Assign a variable to the file to load and the path
+file_to_load = "Resources/election_results.csv"
+
+# Open and read
+election_data = open(file_to_load, 'r')
+
+# To do : perform analysis
+
+# Close the file
+election_data.close()
+###
+# Assign a variable to the file to load and the path
+file_to_load = "Resources/election_results.csv"
+# Open election results and read the file
+with open(file_to_load) as election_data:
+
+    # To do: Perform Analysis
+    print(election_data)
+###
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_anlysis.txt")
+
+# Use the open statement to open the file as a text file.
+outfile = open(file_to_save, "w")
+# Write some datat to the file. 
+outfile.write("Hello World")
+
+#Close the file
+outfile.close()
+'''
 # Import dependencies
 import csv
 import os
@@ -24,12 +61,4 @@ with open(file_to_save, "w") as txt_file:
 with open(file_to_load) as election_data:
 
     #To do: read and analyze the data here. 
-    
-    # Read the file object with the reader function
-    file_reader = csv.reader(election_data)
-
-    #Print the header row.
-    headers = next(file_reader)
-    print(headers)
-    
     
